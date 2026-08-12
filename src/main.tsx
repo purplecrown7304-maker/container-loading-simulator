0819249c0ad7a5d890520ce46c874298cc3e682f
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-08-12 13:05:10 +0900

    feat: mount Excel import actions

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ExcelImportActions from './ExcelImportActions';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <ExcelImportActions />
   </React.StrictMode>,
 );
