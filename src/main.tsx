c73661e85c203e0684557eaa52e07402b42936de
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-08-12 16:03:46 +0900

    style: load integrated mode styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import ExcelImportActions from './ExcelImportActions';
 import './styles.css';
+import './mode.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
