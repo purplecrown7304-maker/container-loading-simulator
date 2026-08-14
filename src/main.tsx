aa6c20982650d7500672d8adcf6631ea4bc7becf
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-08-14 23:07:22 +0900

    feat: wrap app with error recovery boundary

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import ErrorBoundary from './ErrorBoundary';
 import ExcelImportActions from './ExcelImportActions';
 import './styles.css';
 import './mode.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
-    <ExcelImportActions />
+    <ErrorBoundary>
+      <App />
+      <ExcelImportActions />
+    </ErrorBoundary>
   </React.StrictMode>,
 );
