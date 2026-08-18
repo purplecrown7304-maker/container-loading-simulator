9519c9bcddd79e8f44351623ddf64d5a77f14fd2
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-08-18 21:44:56 +0900

    feat: mount cargo search filter bar

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import App from './App';
 import ErrorBoundary from './ErrorBoundary';
 import ExcelImportActions from './ExcelImportActions';
 import LocationSelectionBridge from './LocationSelectionBridge';
+import CargoFilterBar from './CargoFilterBar';
 import './styles.css';
 import './mode.css';
 import './error.css';
 import './selection.css';
+import './cargo-filter.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
@@ -15,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
       <App />
       <ExcelImportActions />
       <LocationSelectionBridge />
+      <CargoFilterBar />
     </ErrorBoundary>
   </React.StrictMode>,
 );
