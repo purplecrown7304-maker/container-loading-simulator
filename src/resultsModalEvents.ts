0242d6e04c2de5e5168c995f9bf7a1b360727f1f
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-08-25 16:06:15 +0900

    feat: attach inertia certification to final results

--- src/resultsModalEvents.ts
+++ src/resultsModalEvents.ts
@@ -1,4 +1,5 @@
 import type { CargoItem, ContainerSpec, LoadingResult } from './engine/types';
+import type { InertiaCertification } from './inertiaCertification';
 
 export const OPEN_RESULTS_MODAL_EVENT = 'container-loading-open-results-modal';
 
@@ -6,6 +7,7 @@ export type ResultsModalDetail = {
   container: ContainerSpec;
   cargo: CargoItem[];
   result: LoadingResult;
+  certification?: InertiaCertification;
 };
 
 export function openResultsModal(detail: ResultsModalDetail) {
