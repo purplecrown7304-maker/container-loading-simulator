3cccc96386e0f888bfbe8b0a1471ab8bbebbc58d
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-08-26 14:07:19 +0900

    feat: switch direct box viewer for truck mode

--- src/BoxLoadingViewer.tsx
+++ src/BoxLoadingViewer.tsx
@@ -1,1 +1,9 @@
-export { default } from './BoxLoadingViewerReference';
+import BoxLoadingViewerReference from './BoxLoadingViewerReference';
+import TruckBoxLoadingViewer from './TruckBoxLoadingViewer';
+import type { ContainerSpec, LoadingResult } from './engine/types';
+
+export default function BoxLoadingViewer({ result, container }: { result: LoadingResult; container: ContainerSpec }) {
+  return container.transportKind === 'truck'
+    ? <TruckBoxLoadingViewer result={result} container={container} />
+    : <BoxLoadingViewerReference result={result} container={container} />;
+}
