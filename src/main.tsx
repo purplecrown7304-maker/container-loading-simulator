a8bd7d15231f54a0808ee0a6d15add82a248fd1b
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-08-28 10:21:25 +0900

    feat: mount transport equipment selector

--- src/main.tsx
+++ src/main.tsx
@@ -19,6 +19,7 @@ import EnterprisePackagingStrategyExplorer from './EnterprisePackagingStrategyEx
 import EnterpriseCartonApprovalCenter from './EnterpriseCartonApprovalCenter';
 import EnterpriseManufacturingSettings from './EnterpriseManufacturingSettings';
 import ReferenceWorkspaceBar from './ReferenceWorkspaceBar';
+import TransportEquipmentSelector from './TransportEquipmentSelector';
 import PhysicsValidationTool from './PhysicsValidationTool';
 import InertiaTestTool from './InertiaTestTool';
 import InertiaTestLauncher from './InertiaTestLauncher';
@@ -58,11 +59,13 @@ import './enterprise-packaging.css';
 import './enterprise-strategy.css';
 import './enterprise-approval.css';
 import './enterprise-manufacturing.css';
+import './transport-equipment.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ErrorBoundary>
       <ReferenceWorkspaceBar />
+      <TransportEquipmentSelector />
       <ResultsOverlay />
       <CertificationInvalidationBridge />
       <FinalCertificationGate />
