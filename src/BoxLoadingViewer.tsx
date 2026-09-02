ad9ff0d508f19fd3c049d1711be7175f6254763a
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-09-02 10:42:22 +0900

    Publish box loading snapshot for remaining length

--- src/BoxLoadingViewer.tsx
+++ src/BoxLoadingViewer.tsx
@@ -1,1 +1,25 @@
-export { default } from './BoxLoadingViewerEquipment';
+import { useEffect } from 'react';
+import type { ContainerSpec, LoadingResult } from './engine/types';
+import BoxLoadingViewerEquipment from './BoxLoadingViewerEquipment';
+import { BOX_VIEW_SNAPSHOT_EVENT } from './RemainingLengthIndicator';
+
+type Props = { result: LoadingResult; container: ContainerSpec };
+type SnapshotWindow = Window & {
+  __containerLoadingBoxViewSnapshot?: Props;
+};
+
+export default function BoxLoadingViewer(props: Props) {
+  useEffect(() => {
+    const publish = () => {
+      (window as SnapshotWindow).__containerLoadingBoxViewSnapshot = props;
+      window.dispatchEvent(new CustomEvent<Props>(BOX_VIEW_SNAPSHOT_EVENT, { detail: props }));
+    };
+    publish();
+    return () => {
+      (window as SnapshotWindow).__containerLoadingBoxViewSnapshot = undefined;
+      window.dispatchEvent(new CustomEvent<undefined>(BOX_VIEW_SNAPSHOT_EVENT, { detail: undefined }));
+    };
+  }, [props.container, props.result]);
+
+  return <BoxLoadingViewerEquipment {...props} />;
+}
