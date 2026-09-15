aee2a02e8456768ebbc0d4eff02b5a36c0552142
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-09-15 13:55:34 +0900

    feat: mount verified loading orchestrator

--- src/main.tsx
+++ src/main.tsx
@@ -50,6 +50,7 @@ import ProductToolsCenter from './ProductToolsCenter';
 import ProductMenuActions from './ProductMenuActions';
 import EquipmentVisualAdminEditor from './EquipmentVisualAdminEditor';
 import WorkflowIntegrationBridge from './WorkflowIntegrationBridge';
+import LoadingRunOrchestrator from './LoadingRunOrchestrator';
 import './tokens.css';
 import './styles.css';
 import './mode.css';
@@ -109,6 +110,7 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
     <ErrorBoundary>
       <RuntimeDiagnosticRecorder />
       <WorkflowIntegrationBridge />
+      <LoadingRunOrchestrator />
       <ReferenceWorkspaceBar />
       <ProductMenuActions />
       <ProductToolsCenter />
