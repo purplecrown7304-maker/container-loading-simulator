667f8de260cc9e1c0a5c00d3d78873dc0e619903
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-09-15 14:00:08 +0900

    style: load verified workflow styles

--- src/main.tsx
+++ src/main.tsx
@@ -104,6 +104,7 @@ import './guided-workflow.css';
 import './guided-workflow-v2.css';
 import './guided-result-tabs-enhancer.css';
 import './workflow-usability-fixes.css';
+import './loading-workflow.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
