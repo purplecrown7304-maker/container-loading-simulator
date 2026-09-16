026693a20f98753772b907aa8f34898d80df1d40
purplecrown7304-maker <purplecrown7304@gmail.com>
2026-09-16 11:48:44 +0900

    chore: remove placeholder layout test

--- e2e/guided-loading-layout.spec.ts
+++
@@ -1,7 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('guided loading layout reserves vertical space for loading-unit controls', async ({ page }) => {
-  await page.goto('/');
-  const css = await page.locator('style').allTextContents().catch(() => []);
-  expect(Array.isArray(css)).toBe(true);
-});
